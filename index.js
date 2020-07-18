d02cf1beb809311b00d7a6229da24f774a28333f
jocke-champ <jockeeee.s@gmail.com>
2020-07-18 14:18:13 +0200

    nya uppdateringar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client(); //botm för bot message
 const { prefix, token } = require('./config.json');
+var fs = require('fs');
+
 // när botten är redo så loggar den det
 bot.once('ready', () => {
     console.log('Redo!');
@@ -24,11 +26,24 @@ bot.on('message', (msg) => {
         msg.channel.send('tok');
     }
     */
-    if(msg.content.startsWith((`${prefix}Karaktär`) && (`${prefix}karaktär`))){
-        text = msg.content.slice(prefix.length).trim().split(' ');
-        for(var i = 1; i < text.length; i++){
-            msg.channel.send('\n' + text[i]);
+    if(msg.content.startsWith((`${prefix}Karaktär`) && (`${prefix}karaktär`))){ //Letar efter '!'karaktärer
+        var text = msg.content.slice(prefix.length).trim().split(' '); // text blir en array meningen som skrevs, tar bort '!'.
+        var namn = text[1]; //namn blir andra elementet i arrayen (namnet)
+        var dndtext = ['Spelare:', 'Spel: ', 'Namn: ', 'Ras: ', 'Ålder: ' ];
+        var logger = fs.createWriteStream(namn + '.txt', { 
+            flags: 'a'
+        });
+        for(var i = 1; i < text.length; i++){ // for-loop som tar sig igenom arrayen som skapades i rad 30
+            var fileContent = '\n' + dndtext[i-1] + text[i]; // eftersom text[0] är 'karaktärer' så startar arrayen 'text' med [1], men dndtext startar med i-1 = 0 vid start
+            msg.channel.send(fileContent);
+            logger.write(fileContent); //startar write-funktioner ur logger, vilket startar en writestream som tilåter texten i for-loopen att skrivas
+            
         }
+        const file = new Discord.MessageAttachment(namn +'.txt'); // skapar ett 'attachment' med namnet på personen  [1]
+        const Embed = {
+            title: namn + 's fil'
+        };
+        msg.channel.send({files: [file], embed: Embed}); // skickar 'attachment' och 'Embed' till discord-kanalen som anropade
     }
     
 });
