3e77554ca2e62d4345537f42feb1ea41355af045
jocke-champ <jockeeee.s@gmail.com>
2020-07-23 13:13:00 +0200

    bot-token

--- index.js
+++ index.js
@@ -53,4 +53,5 @@ bot.on('message', (msg) => {
 
 //Loggar in på Discord med app token som ligger i config.json
 //bot.login(token);
-bot.login(process.env.token);
+//bot.login(process.env.token);
+bot.login(process.env.BOT_TOKEN);
